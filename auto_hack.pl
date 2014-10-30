a22a8b48c4a1b325d014c21cc5a48c18d1bab01f
김준호 <junho85@daumcorp.com>
2014-10-30 14:57:22 +0900

    separate configuration
    separate configuration file
    
    remove grep command
    remove ANSI module

--- auto_hack.pl
+++ auto_hack.pl
@@ -4,14 +4,14 @@ use warnings;
 use strict;
 use utf8;
 
-use Term::ANSIColor;
+use Config::IniFiles;
+use Data::Dumper;
+
 use POSIX;
 
 my $cnt = 1;
 
-my $S2_ID = '8647374e';
-my $NOTE2_ID = '42f60b19af259f7f';
-my $NEXUS_ID = '0149A8CD0F01100E';
+my $cfg = new Config::IniFiles(-file => "config.ini");
 
 while (1) {
 
@@ -30,8 +30,7 @@ while (1) {
 	if ($@) {
 		die unless $@ eq "alarm\n"; # propagate unexpected errors
 		# timed out
-		print colored("error", 'bold red on_blue');
-
+		print "error";
 	} else {
 		# didn't
 	}
@@ -40,13 +39,10 @@ while (1) {
 
 sub hack {
 
-	print colored("[".get_datetime()."] try $cnt start", 'bold yellow on_magenta');
+	print "[".get_datetime()."] try $cnt start";
 	print "\n";
 
 	my @devices = (
-		#$S2_ID,
-		#$NOTE2_ID,
-		#$NEXUS_ID,
 	);
 
 	# device check
@@ -55,7 +51,7 @@ sub hack {
 
 	foreach my $line (@device_info_lines) {
 		chomp($line);
-		print $line."\n";
+		#print $line."\n";
 
 		if ($line =~ /^\*/) {
 			#print "pass $line\n";
@@ -65,20 +61,18 @@ sub hack {
 			next;
 		} elsif ($line =~ /(\S+)\t(device)/) {
 			my $device_id = $1;
-			print "1:$1 2:$2\n";
+			my $type = $2;
+			print "device_id=$device_id; type=$type\n";
 
-			if ($device_id eq $S2_ID) {
-				push @devices, $S2_ID;
+			#print "cfg=";
+			#print Dumper(\$cfg);
+			#print "\n";
+			my $section = get_section_by_id($cfg, $device_id);
+			print "section=$section\n";
+			if ($section) {
+				print "add device $device_id\n";
+				push @devices, $device_id;
 			}
-
-			if ($device_id eq $NOTE2_ID) {
-				push @devices, $NOTE2_ID;
-			}
-
-			if ($device_id eq $NEXUS_ID) {
-				push @devices, $NEXUS_ID;
-			}
-
 		} else {
 			print "device not found or error: $line\n";
 		}
@@ -88,37 +82,7 @@ sub hack {
 
 		print "start $device_id\n";
 
-		my $portal_x = 0;
-		my $portal_y = 0;
-
-		my $hack_x = 0;
-		my $hack_y = 0;
-
-		if ($device_id eq $S2_ID) {
-			print colored("S2", 'bold yellow');
-			print "\n";
-			$portal_x = 200;
-			$portal_y = 600;
-
-			$hack_x = 400;
-			$hack_y = 300;
-		} elsif ($device_id eq $NOTE2_ID) {
-			print colored("NOTE 2", 'bold yellow');
-			print "\n";
-			$portal_x = 350;
-			$portal_y = 900;
-
-			$hack_x = 500;
-			$hack_y = 500;
-		} elsif ($device_id eq $NEXUS_ID) {
-			print colored("NEXUS", 'bold yellow');
-			print "\n";
-			$portal_x = 300;
-			$portal_y = 900;
-
-			$hack_x = 500;
-			$hack_y = 400;
-		}
+		my ($portal_x, $portal_y, $hack_x, $hack_y) = get_positions($device_id);
 
 		print "$device_id screen turn on\n";
 		my $screen = screen_control($device_id, "on");
@@ -161,17 +125,44 @@ sub hack {
 	sleep(300);
 }
 
+sub get_section_by_id {
+	my $cfg = shift;
+	my $id = shift;
+
+	foreach my $section ($cfg->Sections) {
+		if ($id eq $cfg->val($section, 'id')) {
+			return $section;
+		}
+	}
+}
+
+sub get_positions {
+	my $device_id = shift;
+
+	my $section = get_section_by_id($cfg, $device_id);
+
+	my $portal_x = $cfg->val($section, 'portal_x');
+	my $portal_y = $cfg->val($section, 'portal_y');
+
+	my $hack_x = $cfg->val($section, 'hack_x');
+	my $hack_y = $cfg->val($section, 'hack_y');
+	
+	return ($portal_x, $portal_y, $hack_x, $hack_y);
+}
+
 sub screen_check {
 	my $device_id = shift;
 
-	my $result = `adb -s $device_id shell dumpsys input_method | grep mScreenOn`;
+	my @lines = `adb -s $device_id shell dumpsys input_method`;
 	
-	if ($result =~ /mScreenOn=true/) {
-		return "on";
-	} elsif ($result =~ /mScreenOn=false/) {
-		return "off";
-	} else {
-		return "error";
+	foreach my $line (@lines) {
+		if ($line =~ /mScreenOn=true/) {
+			return "on";
+		} elsif ($line =~ /mScreenOn=false/) {
+			return "off";
+		} else {
+			return "error";
+		}
 	}
 }
 
@@ -190,4 +181,3 @@ sub screen_control {
 sub get_datetime {
 	return strftime("%Y-%m-%d %H:%M:%S", localtime(time));
 }
-
